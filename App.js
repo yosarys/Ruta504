5af7bb96ad81ebd2541e6c1182f0470eb0c2b6b0
Yosary <yl_santosp@unicah.edu>
2021-07-26 00:29:29 -0600

    Delete

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import MyStack from './src/components/navegation/Stack';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return ( <
-        MyStack / >
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
