b070ec4b9115fd569c9822e00097e1be11314876
Yosary <yl_santosp@unicah.edu>
2021-07-26 00:00:39 -0600

    Creacion  y Navegacion de Pantallas

---
+++ src/components/screens/Places.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text} from 'react-native';
+
+
+function Places() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Lugares Turisticos</Text>
+      </View>
+    );
+  }
+  export default Places;
