4912e894413c7b7c65d5f8560eca146d63ce27e0
Yosary <yl_santosp@unicah.edu>
2021-07-26 23:41:32 -0600

    Formularios Iniciar Sesion, Registrarse

---
+++ src/components/forms/SigninForm.js
@@ -0,0 +1,68 @@
+import React, { useState, useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Caption, Text, TextInput } from "react-native-paper";
+
+function SigninForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [error, setError] = useState("");
+
+  function handleVerify(input) {
+    if (input === "email") {
+      if (!email) setEmailError(true);
+      else setEmailError(false);
+    } else if (input === "password") {
+      if (!password) setPasswordError(true);
+      else setPasswordError(false);
+    } else if (input === "signin") {
+      if (email && password && !emailError && !passwordError) {
+        signin(email, password);
+      }
+    }
+  }
+
+  return (
+    <View>
+      {state.errorMessage != null && <Text>{state.errorMessage}</Text>}
+      <TextInput
+        mode="outlined"
+        label="Email"
+        autoCapitalize="none"
+        onChangeText={setEmail}
+        value={email}
+        onBlur={() => handleVerify("email")}
+      />
+      {emailError && (
+        <Caption>Por favor ingresa tu cuenta de correo electrónico</Caption>
+      )}
+      <TextInput
+        mode="outlined"
+        label="Password"
+        autoCapitalize="none"
+        secureTextEntry
+        onChangeText={setPassword}
+        value={password}
+        onBlur={() => handleVerify("password")}
+      />
+      {passwordError && <Caption>Por favor ingresa tu contraseña</Caption>}
+      <Button
+        mode="contained"
+        style={styles.button}
+        onPress={() => handleVerify("signin")}
+      >
+       Iniciar Sesion
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+});
+
+export default SigninForm;
---
+++ src/components/forms/SignupForm.js
@@ -0,0 +1,120 @@
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Caption, Text, TextInput } from "react-native-paper";
+import { validate } from "email-validator";
+
+function SignupForm({ navigation }) {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [fullnameError, setFullnameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (state.registered) navigation.navigate("Home");
+  }, [state.registered]);
+
+  function handleVerify(input) {
+    if (input === "Nombre Completo") {
+      if (!fullname) setFullnameError(true);
+      else setFullnameError(false);
+    } else if (input === "Correo electronico") {
+      if (!email) setEmailError(true);
+      else if (!validate(email)) setEmailError(true);
+      else setEmailError(false);
+    } else if (input === "Contraseña") {
+      if (!password) setPasswordError(true);
+      else if (password.length < 6) setPasswordError(true);
+      else setPasswordError(false);
+    } else if (input === "Confirma Contraseña") {
+      if (!confirmPassword) setConfirmPasswordError(true);
+      else if (password !== confirmPassword) setConfirmPasswordError(true);
+      else setConfirmPasswordError(false);
+    } else if (input === "Registrate") {
+      if (
+        fullname &&
+        email &&
+        password &&
+        confirmPassword &&
+        !fullnameError &&
+        !emailError &&
+        !passwordError &&
+        !confirmPasswordError
+      ) {
+        try {
+          signup(fullname, email, password);
+        } catch (error) {
+          console.log(error);
+        }
+      } else setError("Todos los campos son requeridos!");
+    }
+  }
+
+  return (
+    <View>
+      {error && <Text>{error}</Text>}
+      {state.errorMessage != null && <Text>{state.errorMessage}</Text>}
+      <TextInput
+        mode="outlined"
+        label="Nombre Completo"
+        value={fullname}
+        onChangeText={setFullname}
+        onBlur={() => handleVerify("Nombre Completo")}
+      />
+      {fullnameError && <Caption>Please enter your name</Caption>}
+      <TextInput
+        mode="outlined"
+        label="correo electronico"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        onBlur={() => handleVerify("correo electronico")}
+      />
+      {emailError && <Caption>Por favor, escriba su correo electronico</Caption>}
+      <TextInput
+        mode="outlined"
+        label="Contraseña"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        secureTextEntry
+        onBlur={() => handleVerify("Contraseña")}
+      />
+      {passwordError && (
+        <Caption>por favor, Ingrese una contraseña valida. Minimo 6 caracteres</Caption>
+      )}
+      <TextInput
+        mode="outlined"
+        label="Confirme su contraseña"
+        value={confirmPassword}
+        onChangeText={setConfirmPassword}
+        autoCapitalize="none"
+        secureTextEntry
+        onBlur={() => handleVerify("Confirme su contraseña")}
+      />
+      {confirmPasswordError && (
+        <Caption>Por favor, escriba su confirmacion de contraseña</Caption>
+      )}
+      <Button
+        mode="contained"
+        style={styles.button}
+        onPress={() => handleVerify("signup")}
+      >
+        Crear Cuenta
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+});
+
+export default SignupForm;
