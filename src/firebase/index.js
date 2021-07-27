36c13a5d52967ce61b4c251683c9a8545d29cb8e
nicollh9 <nicollarita06@gmail.com>
2021-07-26 22:40:19 -0600

    Conexión a firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+import "@firebase/auth";
+import "@firebase/firestore";
+import getEnvVars from "../../environment";
+
+const {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} = getEnvVars();
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+};
+
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+
+export { firebase };
