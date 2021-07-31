7d39dca1d59b6ae3c84a358544dd5e4b4e39ae3e
nicollh9 <nicollarita06@gmail.com>
2021-07-31 16:52:37 -0600

    Creación de Modal en pantalla Home

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,9 +1,11 @@
-import React, { useContext } from "react";
-import { View,StyleSheet, TouchableOpacity, Text} from 'react-native';
+import React, { useContext, useState} from "react";
+import { View,StyleSheet, TouchableOpacity,SafeAreaView} from 'react-native';
+import {Modal,Portal, IconButton,Text} from 'react-native-paper';
 import { Context as AuthContext } from "../../providers/AuthContext";
 
 function Home({ navigation }) {
 const { signout } = useContext(AuthContext);
+const [modal, setModal] = useState(false); 
 
     return (
       <View style={styles.container}> 
@@ -26,6 +28,28 @@ const { signout } = useContext(AuthContext);
            onPress= {signout}>
           <Text style={styles.text}>Cerrar Sesion</Text>
         </TouchableOpacity> 
+
+        <IconButton
+        icon="cog"
+        color='blue'
+        size={30}
+        onPress={() => setModal(true)}/>
+
+      <Portal>
+        <Modal visible={modal} style={styles.modal}>
+            <SafeAreaView>
+              <IconButton 
+                icon="close"
+                size={25}
+                onPress={() => setModal(false)}/>
+              <View>
+                <Text>
+                  Ruta504
+                </Text>
+              </View>
+            </SafeAreaView>
+          </Modal>
+      </Portal>
 
       </View>
     );
@@ -53,5 +77,13 @@ const { signout } = useContext(AuthContext);
     textAlign:"center", 
     margin:30,
     color: 'white',
+  },
+  modal: {
+    margin: 20,
+    marginTop:1,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
   },
 });
